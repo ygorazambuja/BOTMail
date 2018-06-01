3ff5c20050f82a50040f3e6e7991e93abeb319ac
Ygor Azambuja <ygorazambuja@gmail.com>
2018-05-31 22:30:42 -0400

    Otimizado.

--- src/main/java/BotmailController.java
+++ src/main/java/BotmailController.java
@@ -3,24 +3,30 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
-import javafx.scene.image.ImageView;
+import org.apache.commons.mail.DefaultAuthenticator;
+import org.apache.commons.mail.EmailAttachment;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.MultiPartEmail;
 
+import javax.swing.*;
+import javax.swing.filechooser.FileSystemView;
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class BotmailController implements Initializable {
 
     @FXML
-    private JFXTextField emailTextField, destinoTextField, assuntoTextField, infoTextField;
+    public JFXButton btnAnexo;
+
+    @FXML
+    private JFXTextField emailTextField, destinoTextField, assuntoTextField, infoTextField, anexoTextField;
 
     @FXML
     private JFXTextArea textArea;
 
     @FXML
     private JFXProgressBar progressBar;
-
-    @FXML
-    private ImageView btnAnexo;
 
     @FXML
     private JFXPasswordField passwordTextField;
@@ -37,9 +43,18 @@ public class BotmailController implements Initializable {
     @FXML
     private JFXListView<String> listView = new JFXListView<>();
 
+
     @FXML
     void btnAnexo(ActionEvent event) {
 
+        JFileChooser jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+
+        int returnValue = jfc.showOpenDialog(null);
+
+        if (returnValue == JFileChooser.APPROVE_OPTION) {
+            File selectedFile = jfc.getSelectedFile();
+            anexoTextField.setText(selectedFile.getAbsolutePath());
+        }
     }
 
     @FXML
@@ -95,7 +110,7 @@ public class BotmailController implements Initializable {
         validarComeco();
     }
 
-    void validarComeco() {
+    private void validarComeco() {
         if (checkBoxEmail.isSelected() && checkBoxSenha.isSelected() && nEmails.getValue() >= 1) {
             btnComeco.setVisible(true);
         }
@@ -105,28 +120,90 @@ public class BotmailController implements Initializable {
 
         Integer formatado = (int) nEmails.getValue();
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
+        if (anexoTextField.getText() == null) {
+            // então não tem anexo
+
+            new Thread(() -> {
                 progressBar.setVisible(true);
 
-                for (String s : listView.getItems()) {
-                    SendEmail sendEmail = new SendEmail("smtp.gmail.com", emailTextField.getText(), passwordTextField.getText(), "465", true, true);
-                    sendEmail.sendSimpleEmail(s, assuntoTextField.getText(), textArea.getText());
-                    System.out.println("Email sendo mandado para : " + s);
+                for (int i = 0; i < formatado; i++) {
+
+
+                    for (String destinatarios : listView.getItems()) {
+                        SendEmail sendEmail = new SendEmail("smtp.gmail.com", emailTextField.getText(), passwordTextField.getText(), "465", true, true);
+                        sendEmail.sendSimpleEmail(destinatarios, assuntoTextField.getText(), textArea.getText());
+                        System.out.println("Email sendo mandado para : " + destinatarios);
+                    }
                 }
                 infoTextField.setDisable(false);
                 infoTextField.setVisible(true);
+
                 infoTextField.setText("Um total de " + (formatado) + " emails foram enviados com sucesso!");
                 progressBar.setVisible(false);
-            }
-        }).start();
+
+            }).start();
+
+        } else {
+            // tem anexo
+            new Thread(() -> {
+                progressBar.setVisible(true);
+                for (int i = 0; i < formatado; i++) {
 
 
+                    for (String destinatarios : listView.getItems()) {
+
+                        EmailAttachment anexo = new EmailAttachment();
+                        anexo.setPath(anexoTextField.getText());
+                        anexo.setDisposition(EmailAttachment.ATTACHMENT);
+                        anexo.setDescription("");
+                        anexo.setName("");
+
+                        MultiPartEmail email = new MultiPartEmail();
+
+                        email.setHostName("smtp.gmail.com");
+                        email.setSmtpPort(465);
+                        email.setAuthenticator(new DefaultAuthenticator(emailTextField.getText(), passwordTextField.getText()));
+                        email.setSSLOnConnect(true);
+
+                        try {
+                            email.setFrom(emailTextField.getText());
+                        } catch (EmailException e) {
+                            e.printStackTrace();
+                        }
+
+                        email.setSubject(assuntoTextField.getText());
+
+                        try {
+                            email.setFrom(emailTextField.getText());
+                        } catch (EmailException e) {
+                            e.printStackTrace();
+                        }
+
+                        try {
+                            email.addTo(destinatarios);
+                        } catch (EmailException e) {
+                            e.printStackTrace();
+                        }
 
 
+                        try {
+                            email.setMsg(textArea.getText());
+                            try {
+                                email.attach(anexo);
+                                email.send();
+                            } catch (EmailException e) {
+                                e.printStackTrace();
+                            }
+                        } catch (EmailException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+                progressBar.setVisible(false);
+            }).start();
+
+        }
     }
-
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
